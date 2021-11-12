cd71699330dff2d6f2757fc0ef7675b42f8c97de
davebhavin9 <dave.b@northeastern.edu>
2021-11-12 10:25:20 -0500

    initial commit

---
+++ Sale_ Discount and Clearance Watches, Handbags, Wallets & More - Fossil_files/PowerReviews-Config.js
@@ -0,0 +1,11 @@
+
+
+
+
+window.POWER_REVIEWS_CONFIG = {
+locale: "null",
+merchant_group_id: "49928",
+merchant_id: "625350",
+api_key: "526ad0c8-1beb-41ce-9215-1d0d02d6bb54",
+review_wrapper_url: "https://www.fossil.com/on/demandware.store/Sites-fossil-na-Site/en_US/PowerReviews-WriteReviewPage?pr_page_id="
+};
---
+++ Sale_ Discount and Clearance Watches, Handbags, Wallets & More - Fossil_files/config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C4N32QFM5GFN4SM5RRGG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true});
+window[window["TiktokAnalyticsObject"]].instance("C4N32QFM5GFN4SM5RRGG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C4N32QFM5GFN4SM5RRGG").setPixelInfo({status: 0, name: "Fossil Group NAM Pixel", advertiserID: "7000493836233408513", setupMode: 1, partner: "", is_onsite: false});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("fossil.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
